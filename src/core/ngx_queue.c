f9068fc5eb02a997b1b8259bd9412cbd5c9827da
dingrui <Bannirui@outlook.com>
2025-03-06 19:00:31 +0800

    双链表

--- src/core/ngx_queue.c
+++ src/core/ngx_queue.c
@@ -17,20 +17,23 @@ static void ngx_queue_merge(ngx_queue_t *queue, ngx_queue_t *tail,
  * find the middle queue element if the queue has odd number of elements
  * or the first element of the queue's second part otherwise
  */
-
+/*
+ * 快慢指针方式找链表的中间元素
+ */
 ngx_queue_t *
 ngx_queue_middle(ngx_queue_t *queue)
 {
     ngx_queue_t  *middle, *next;
-
+    // 链表中首元素
     middle = ngx_queue_head(queue);
-
+    // 链表中末元素
     if (middle == ngx_queue_last(queue)) {
+        // 链表中只有一个元素
         return middle;
     }
-
+    // 首元素
     next = ngx_queue_head(queue);
-
+    // 快慢指针 middle慢指针 next快指针
     for ( ;; ) {
         middle = ngx_queue_next(middle);
 
@@ -51,6 +54,10 @@ ngx_queue_middle(ngx_queue_t *queue)
 
 /* the stable merge sort */
 
+/**
+ * 归并排序
+ * @param queue 双链表的哨兵节点
+ */
 void
 ngx_queue_sort(ngx_queue_t *queue,
     ngx_int_t (*cmp)(const ngx_queue_t *, const ngx_queue_t *))
@@ -60,11 +67,17 @@ ngx_queue_sort(ngx_queue_t *queue,
     q = ngx_queue_head(queue);
 
     if (q == ngx_queue_last(queue)) {
+        // 队列只有1个元素
         return;
     }
-
+    // 队列中间元素
     q = ngx_queue_middle(queue);
-
+    /*
+     * 一分为二
+     * queue是前半部分 [...q)
+     * tail是后半部分 [q...]
+     * 分别对前后两部分分别排序 然后合并
+     */
     ngx_queue_split(queue, q, &tail);
 
     ngx_queue_sort(queue, cmp);
@@ -73,34 +86,40 @@ ngx_queue_sort(ngx_queue_t *queue,
     ngx_queue_merge(queue, &tail, cmp);
 }
 
-
+/*
+ * 合并升序链表 tail上节点都合并到queue上 保持升序
+ * @param queue 链表
+ * @param tail 链表
+ * @param cmp 比较函数
+ */
 static void
 ngx_queue_merge(ngx_queue_t *queue, ngx_queue_t *tail,
     ngx_int_t (*cmp)(const ngx_queue_t *, const ngx_queue_t *))
 {
     ngx_queue_t  *q1, *q2;
-
+    /*
+     * 遍历两条链表合并到queue上
+     */
     q1 = ngx_queue_head(queue);
     q2 = ngx_queue_head(tail);
-
     for ( ;; ) {
         if (q1 == ngx_queue_sentinel(queue)) {
+            // 第1条链表空了 第2条链表所有数据节点都挂到第1条链表上
             ngx_queue_add(queue, tail);
             break;
         }
-
         if (q2 == ngx_queue_sentinel(tail)) {
+            // 第2条链表空了
             break;
         }
-
         if (cmp(q1, q2) <= 0) {
+            // 继续遍历
             q1 = ngx_queue_next(q1);
             continue;
         }
-
+        // 从第2条链表上移除节点 插入到第1条链表节点前驱
         ngx_queue_remove(q2);
         ngx_queue_insert_before(q1, q2);
-
         q2 = ngx_queue_head(tail);
     }
 }
--- src/core/ngx_queue.h
+++ src/core/ngx_queue.h
@@ -15,21 +15,32 @@
 
 typedef struct ngx_queue_s  ngx_queue_t;
 
+/*
+ * 双链表
+ */
 struct ngx_queue_s {
+    // 前驱
     ngx_queue_t  *prev;
+    // 后继
     ngx_queue_t  *next;
 };
 
-
+// 实例化好后进行初始化 头节点是哨兵节点 不存储数据
 #define ngx_queue_init(q)                                                     \
     (q)->prev = q;                                                            \
     (q)->next = q
 
 
+// 队列为空
 #define ngx_queue_empty(h)                                                    \
     (h == (h)->prev)
 
 
+/*
+ * 头插入队
+ * @param h 队列
+ * @param x 节点入队
+ */
 #define ngx_queue_insert_head(h, x)                                           \
     (x)->next = (h)->next;                                                    \
     (x)->next->prev = x;                                                      \
@@ -40,32 +51,42 @@ struct ngx_queue_s {
 #define ngx_queue_insert_after   ngx_queue_insert_head
 
 
+/*
+ * 尾插入队 本质是把x插入到h的前驱
+ * <ul>
+ *   <li>当h是哨兵时 结果就是x被尾插到h链表的末数据节点</li>
+ *   <li>是h是数据节点时 结果就是x被插入当作h的前驱节点</li>
+ * </ul>
+ * @param h 队列
+ * @param x 节点入队
+ */
 #define ngx_queue_insert_tail(h, x)                                           \
     (x)->prev = (h)->prev;                                                    \
     (x)->prev->next = x;                                                      \
     (x)->next = h;                                                            \
     (h)->prev = x
 
-
+// 第2个节点插入到第1个节点的前驱
 #define ngx_queue_insert_before   ngx_queue_insert_tail
 
 
+// 队列首元素
 #define ngx_queue_head(h)                                                     \
     (h)->next
 
-
+// 队列末元素
 #define ngx_queue_last(h)                                                     \
     (h)->prev
 
-
+// 队列的哨兵节点
 #define ngx_queue_sentinel(h)                                                 \
     (h)
 
-
+// 后继节点
 #define ngx_queue_next(q)                                                     \
     (q)->next
 
-
+// 前驱节点
 #define ngx_queue_prev(q)                                                     \
     (q)->prev
 
@@ -80,13 +101,21 @@ struct ngx_queue_s {
 
 #else
 
+// 从双端链表中移除数据节点
 #define ngx_queue_remove(x)                                                   \
     (x)->next->prev = (x)->prev;                                              \
     (x)->prev->next = (x)->next
 
 #endif
 
-
+/*
+ * 链表拆分
+ * h [...q) 前半部分以原来的哨兵为链表
+ * n [q...] 后半部分以n为哨兵形成新的链表
+ * @param h 原链表的哨兵节点
+ * @param q 要拆的节点
+ * @param n 新拆出来的链表的哨兵节点
+ */
 #define ngx_queue_split(h, q, n)                                              \
     (n)->prev = (h)->prev;                                                    \
     (n)->prev->next = n;                                                      \
@@ -96,6 +125,15 @@ struct ngx_queue_s {
     (q)->prev = n;
 
 
+/*
+ * 合并两个链表
+ * <ul>
+ *   <li>合并完新链表的哨兵节点为h</li>
+ *   <li>只把n的数据节点部分挂到h上</li>
+ * </ul>
+ * @param h 以h为哨兵的双链表
+ * @param n 以n为哨兵的双链表
+ */
 #define ngx_queue_add(h, n)                                                   \
     (h)->prev->next = (n)->next;                                              \
     (n)->next->prev = (h)->prev;                                              \
