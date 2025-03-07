83469a9a0424acf8b4c73b22811135bcb1e3b20e
dingrui <Bannirui@outlook.com>
2025-03-07 17:35:18 +0800

    单链表

--- src/core/ngx_list.c
+++ src/core/ngx_list.c
@@ -8,17 +8,24 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+/*
+ * 实例化链表
+ * 虽然是链表 在创建链表时知道已知要放n个元素 这n个元素大小作为一个链表节点要存放多少个元素
+ * @param pool 内存池
+ * @param n 一个链表节点要放多少个元素
+ * @param size 一个元素多大
+ * @return 链表
+ */
 ngx_list_t *
 ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
     ngx_list_t  *list;
-
+    // 链表结构
     list = ngx_palloc(pool, sizeof(ngx_list_t));
     if (list == NULL) {
         return NULL;
     }
-
+    // 初始化链表
     if (ngx_list_init(list, pool, n, size) != NGX_OK) {
         return NULL;
     }
@@ -26,7 +33,11 @@ ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
     return list;
 }
 
-
+/*
+ * 向链表中添加元素
+ * @param l 链表
+ * @return 调用方拿到内存地址负责写数据
+ */
 void *
 ngx_list_push(ngx_list_t *l)
 {
@@ -36,28 +47,28 @@ ngx_list_push(ngx_list_t *l)
     last = l->last;
 
     if (last->nelts == l->nalloc) {
-
+        // 链表上最后一个节点已经满了 给链表新分配个新节点
         /* the last part is full, allocate a new list part */
 
         last = ngx_palloc(l->pool, sizeof(ngx_list_part_t));
         if (last == NULL) {
             return NULL;
         }
-
+        // 分配用来存放数据的内存=一个链表容纳的元素个数*一个元素大小
         last->elts = ngx_palloc(l->pool, l->nalloc * l->size);
         if (last->elts == NULL) {
             return NULL;
         }
-
+        // 新分配的节点还没放元素 元素数据是0
         last->nelts = 0;
         last->next = NULL;
 
         l->last->next = last;
         l->last = last;
     }
-
+    // last指向的是最后一个有效节点 节点已经存放了nelts个元素 新元素入链表的内存地址
     elt = (char *) last->elts + l->size * last->nelts;
     last->nelts++;
-
+    // 把存放新数据的内存地址交给调用方 让调用方把数据写到内存上去
     return elt;
 }
--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -15,16 +15,39 @@
 
 typedef struct ngx_list_part_s  ngx_list_part_t;
 
+// 单链表节点
 struct ngx_list_part_s {
+    /*
+     * 存放数据的内存起始位置
+     * 元素占用空间总内存是size*nalloc
+     * 那么一个链表节点数据空间是[elts...elts+size*nalloc)
+     */
     void             *elts;
+    // 链表节点上已经存放了多少个元素
     ngx_uint_t        nelts;
     ngx_list_part_t  *next;
 };
 
-
+/*
+ * 单链表
+ * 不同于双链表
+ * <ul>
+ *   <li>双链表维护了哨兵节点 哨兵节点占位用不存数据</li>
+ *   <li>单链表不需要哨兵节点</li>
+ * </ul>
+ */
 typedef struct {
+    // 单链表的尾节点 为什么要维护尾节点呢 用头插就行了
     ngx_list_part_t  *last;
+    // 单链表的头节点
     ngx_list_part_t   part;
+    /*
+     * <ul>
+     *   <li>size 每个元素大小</li>
+     *   <li>nalloc 链表上每个链表节点元素容量</li>
+     * </ul>
+     * size*nalloc就是一个节点上所有元素要占用的空间
+     */
     size_t            size;
     ngx_uint_t        nalloc;
     ngx_pool_t       *pool;
@@ -33,14 +56,23 @@ typedef struct {
 
 ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);
 
+/*
+ * 链表初始化
+ * @param list 实例化好的链表
+ * @param pool 内存池
+ * @param n 一个链表节点的元素容量 一个链表节点存放多少个元素
+ * @param size 一个元素大小
+ * @return 操作状态码
+ */
 static ngx_inline ngx_int_t
 ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
+    // 给链表节点分配内存存放数据
     list->part.elts = ngx_palloc(pool, n * size);
     if (list->part.elts == NULL) {
         return NGX_ERROR;
     }
-
+    // 链表上已经存放的元素个数 还没放数据
     list->part.nelts = 0;
     list->part.next = NULL;
     list->last = &list->part;
